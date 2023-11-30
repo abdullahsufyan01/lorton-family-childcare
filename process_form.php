ac82e60af514fa1427e3c874d70aad92059db779
Abdullah Sufyan <malickabee01@gmail.com>
2023-11-30 14:45:33 +0500

    php funtionality for contact form

---
+++ process_form.php
@@ -0,0 +1,62 @@
+<?php
+$submission_successful = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Your email address where you want to receive emails
+    $to = 'malickabee01@gmail.com';
+
+    // Subject for the email
+    $email_subject = "New Form Submission: $subject";
+
+    // Message body for the email
+    $email_body = "You have received a new form submission:\n\n";
+    $email_body .= "First Name: $fname\n";
+    $email_body .= "Last Name: $lname\n";
+    $email_body .= "Address: $address\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Phone: $phone\n";
+    $email_body .= "Subject: $subject\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Headers for the email
+    $headers = "From: $email";
+
+    // Attempt to send email
+    $mail_success = mail($to, $email_subject, $email_body, $headers);
+
+    // Auto-response to the user
+    if ($mail_success) {
+        $auto_response_subject = "Thank you for your submission";
+        $auto_response_message = "Dear $fname,\n\nThank you for submitting the form. We will get back to you as soon as possible.";
+        mail($email, $auto_response_subject, $auto_response_message, $headers);
+        $submission_successful = true;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Submission</title>
+</head>
+<body>
+
+<?php if ($submission_successful): ?>
+    <p>Thank you for submitting the form!</p>
+<?php else: ?>
+    <p>Form not submitted. Please try again.</p>
+<?php endif; ?>
+
+</body>
+</html>
